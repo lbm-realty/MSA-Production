681e4bd07637932b8db0315b83ed66df8197864b
lbm-realty <labeebmuntasir@gmail.com>
2025-02-01 14:19:52 -0600

    Prayer Hours Component added with working API

--- frontend/src/components/prayerDisplay.jsx
+++ frontend/src/components/prayerDisplay.jsx
@@ -1,36 +1,44 @@
 import "../css/prayerDisplay.css";
 
 const PrayerDisplay = (props) => {
-  const prayers = ["Fajr -", "Sunrise -","Dhuhr -", "Asr -", "Maghreb -", "Isha -"];
+  const prayers = [
+    "Fajr -",
+    "Sunrise -",
+    "Dhuhr -",
+    "Asr -",
+    "Maghreb -",
+    "Isha -",
+  ];
 
   return (
     <>
-      {Array.isArray(props.data) ? (
-        props.data.length > 0 ? (
-          <div className="prayer-outer">
-            <div className="prayer-header-rest">
-              <h2 className="prayer-heading">Prayer Hours</h2>
+      <div className="prayer-outer">
+        <div className="prayer-header-rest">
+          <h2 className="prayer-heading">Prayer Hours</h2>
+
+          {Array.isArray(props.data) ? (
+            props.data.length > 0 ? (
               <div className="prayer-inner">
-              {props.data.map((prayer, index) => (
+                {props.data.map((prayer, index) => (
                   <div className="name-hours">
                     <p id="prayer-name">{prayers[index]}</p>
                     <p id="space-div"></p>
                     <div id="prayer-time">{prayer}</div>
                   </div>
-              ))}
-                </div>
-                </div>
-          </div>
-        ) : (
-          <div>
-            <p>Loading...</p>
-          </div>
-        )
-      ) : (
-        <div>
-          <p>Loading...</p>
+                ))}
+              </div>
+            ) : (
+              <div className="prayer-display-loading-o">
+                <p className="prayer-display-loading-i">Loading...</p>
+              </div>
+            )
+          ) : (
+            <div className="prayer-display-loading-o">
+              <p className="prayer-display-loading-i">Loading...</p>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </>
   );
 };
